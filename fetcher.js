ce0f57213d36f61cf644c951393a19b651792df6
Ben Stolb <benstolb@protonmail.com>
2022-10-27 00:46:49 -0700

    Refactored to use a callback instead

--- fetcher.js
+++ fetcher.js
@@ -5,16 +5,19 @@ const url = input[0];
 const location = input[1];
 // console.log(url);
 // console.log(location);
-
-request(url, (err, response/*I dont know what to do with response */, body) => {
-  if (err) {
-    console.error(err);
-  }
-  // console.log(response); All this does is spit it all out to console. Leaving it out is cleaner.
-  fs.writeFile(location, body, err => {
+const writePageToFile = (site, fileLocation) => {
+  request(site, (err, response/*I dont know what to do with response */, body) => {
     if (err) {
       console.error(err);
     }
-    console.log(`Downloaded and saved ${body.length} bytes to ${location}`);
+    // console.log(response); All this does is spit it all out to console. Leaving it out is cleaner.
+    fs.writeFile(fileLocation, body, err => {
+      if (err) {
+        console.error(err);
+      }
+      console.log(`Downloaded and saved ${body.length} bytes to ${location}`);
+    });
   });
-});
+};
+
+writePageToFile(url, location)
